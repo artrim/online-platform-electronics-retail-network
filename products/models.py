95619f6467a9391ca7cf0bae31b5a17f914c3b6d
Артур Хамидуллин <artrimskiy@gmail.com>
2024-09-23 18:25:13 +0500

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from company.models import Company
+
+
+class Product(models.Model):
+    """Модель продукта"""
+    name = models.CharField(max_length=255, verbose_name='название')
+    product_models = models.CharField(max_length=100, verbose_name='модель')
+    create_date = models.DateField(verbose_name='дата выхода продукта на рынок')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name='компания')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
